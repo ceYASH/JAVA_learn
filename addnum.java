7ea1d524e72832e2dc0ed7a37c55573ee048cca2
ceYASH <yashthakurce@gmail.com>
2025-02-25 10:16:37 +0530

    add number progrm

---
+++ addnum.java
@@ -0,0 +1,10 @@
+public class addnum {
+    public static void main(String[] args){
+        int num1 = 6;
+        int num2 = 7;
+        int num3 = 4;
+        int sum = num1+num2+num3;
+        System.out.println(sum);
+    }
+    
+}
