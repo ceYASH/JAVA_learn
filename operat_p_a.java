91792ac5b079df46b71a46695b73bd29299ad3b1
ceYASH <yashthakurce@gmail.com>
2025-03-03 15:00:20 +0530

    operators a&p

---
+++ operat_p_a.java
@@ -0,0 +1,21 @@
+public class operat_p_a {
+    public static void main(String[] args){
+        int a = 6*5-34/2;
+        System.out.println(a);
+        int b = 60/5-34*2;
+        System.out.println(b);
+
+        System.out.println("Quick Quiz");
+        int x = 6;
+        int y = 1;
+        int k = x*y/2;
+        System.out.println(k);
+
+        int c = 1;
+        int d = 4;
+        int f = 5;
+        int e = c*c - (4*f*d)/(2*f);
+        System.out.println(e);
+    }
+    
+}
