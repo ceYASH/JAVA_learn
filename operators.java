a41bcb5fedc8988bba897d011935df74b79fdaf3
ceYASH <yashthakurce@gmail.com>
2025-03-03 14:40:24 +0530

    different operators

---
+++ operators.java
@@ -0,0 +1,20 @@
+public class operators {
+    public static void main(String[] args){
+        int a = 4;
+        int b = 5;
+        b +=5;
+        System.out.println(b);
+        System.out.println(64>6);
+
+        System.out.println(64>5 && 64>88);
+        System.out.println(64>5 || 64>88);
+
+        System.out.println("bitwise operators");
+        System.out.println(2&3);
+        System.out.println("2 = 10 & 3 = 11");
+        System.out.println(2|3);
+        
+       
+    }
+    
+}
